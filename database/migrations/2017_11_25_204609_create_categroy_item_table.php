fc0afa8c0cceb8ff44d246eef604986e37f141ee
3omar1911 <oa36345@gmail.com>
2017-11-25 22:57:45 +0200

    creating the migration files.

---
+++ database/migrations/2017_11_25_204609_create_categroy_item_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategroyItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categroy_item', function (Blueprint $table) {
+            $table->increments('id');
+             $table->integer('categroy_id')->unsigned();
+            $table->integer('item_id')->unsigned();            
+            $table->timestamps();
+            
+            $table->foreign('categroy_id')->references('id')->on('categories');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categroy_item');
+    }
+}
