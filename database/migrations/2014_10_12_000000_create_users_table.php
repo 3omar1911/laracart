44d372f994e4f780b95961dd1ef83455b6805a6d
3omar1911 <oa36345@gmail.com>
2017-11-25 23:28:58 +0200

    after the migration to the database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,9 +21,13 @@ class CreateUsersTable extends Migration
             $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
-
-            $table->foreign('role_id')->references('id')->on('roles');
         });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('role_id')->refereneces('id')->on('roles');
+        });
+
+
     }
 
     /**
